771eed32dbea2cb46e484a386ead98d8459bf649
Harun-Siyad <harunsiyad@outlook.de>
2023-01-26 16:24:10 +0100

    Update scirpt.sql

--- scirpt.sql
+++ scirpt.sql
@@ -33,7 +33,7 @@ CREATE TABLE `product`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `description` TEXT NOT NULL,
     `price` DECIMAL(6,2),
-    `fk_product_category_id` INT NOT NULL,
+    `fk_product_category_id` INT NOT NULL, 
     PRIMARY KEY (`id`),
     FOREIGN KEY (`fk_product_category_id`) REFERENCES `product_category`(`id`)
 );
@@ -45,9 +45,7 @@ CREATE TABLE `order_has_product` (
   FOREIGN KEY (`fk_product_id`) REFERENCES `product`(`id`)
 );
 
-BEGIN;
-
-INSERT INTO `customer` (`firstname`, `lastname`, `zip`, `location`, `email`, `phone_number`) VALUES
+INSERT INTO `customer` (`firstname`, `lastname`, `postcode`, `location`, `email`, `phone_number`) VALUES
 ('Arlind', 'Sulejmani', '8055', 'Zuerich', 'sulejmaniarlind5@gmail.com', '+41 76 222 22 22'),
 ('Harun', 'Siyad', '8050', 'Zuerich', 'harunsiyad@gmail.com', '+41 76 333 33 33'),
 ('Maximilian', 'Kos', '8912', 'Obfelnden', 'maxkos@gmail.com', '+41 76 444 44 44'),
@@ -55,16 +53,12 @@ INSERT INTO `customer` (`firstname`, `lastname`, `zip`, `location`, `email`, `ph
 ('Max', 'Kos', '8912', 'Obfelnden', 'maxos@gmail.com', '+41 76 666 666 66');
 
 INSERT INTO `product_category` (`name`) VALUES
-("Pizza Margherita"),
-("Schinken"),
-("Vegan");
-
-INSERT INTO `product` (`id`, `description`, `price`, `fk_product_category_id`) VALUES
-(1, 'Smartphone', 500, 1),
-(2, 'Laptop', 1500, 2),
-(3, 'Tablet', 800, 3),
-(4, 'TV', 1000, 1),
-(5, 'Printer', 200, 2);
+("Pizza"),
+("Lasagne"),
+("Vegan"),
+("Vegetarisch"),
+("Pasta"),
+("Getraenke");
 
 INSERT INTO `order` (`amount`, `order_date`, `delivery_date`, `fk_customer_id`) VALUES
 (1, '2022-01-20 19:00:00', '2022-01-20 21:00:00', 1),
